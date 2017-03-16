c0fdcb76ca8cada8781bfe7b0ca4601d26e8f9ac
UrsKlemen <klemen.ursic@student.um.si>
2017-03-16 13:39:29 +0100

    Test

--- app/src/main/java/com/example/klemen/bugreporter/FloatingWindow.java
+++ app/src/main/java/com/example/klemen/bugreporter/FloatingWindow.java
@@ -37,8 +37,7 @@ public class FloatingWindow extends Service{
     boolean clicked = true;
     CountDownTimer countTimer;
     View layoutButton;
-    int lastPositionX, lastPositionY;
-
+    com.github.clans.fab.FloatingActionButton video, screen, description;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -51,16 +50,18 @@ public class FloatingWindow extends Service{
 
         //declaration
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        setTheme(R.style.AppTheme);
         countTimer = null;
         Display display = windowManager.getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         final int width = size.x;
         final int height = size.y;
+        description = new com.github.clans.fab.FloatingActionButton(FloatingWindow.this);
+        screen = new com.github.clans.fab.FloatingActionButton(FloatingWindow.this);
+        video = new com.github.clans.fab.FloatingActionButton(FloatingWindow.this);
 
         //Main Button
-        setTheme(R.style.AppTheme);
-        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         layoutButton = inflater.inflate(R.layout.activity_main_button, null);
         final WindowManager.LayoutParams parametersButton = new WindowManager.LayoutParams(
@@ -69,70 +70,37 @@ public class FloatingWindow extends Service{
                 WindowManager.LayoutParams.TYPE_TOAST,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
-
         parametersButton.gravity = Gravity.RIGHT;
-        parametersButton.x = 0;
-        parametersButton.y = 200;
+        //parametersButton.x = 0;
+        //parametersButton.y = 200;
         windowManager.addView(layoutButton, parametersButton);
 
 
-        //Button touched/moved/clicked
-        try {
-
-            layoutButton.findViewById(R.id.material_design_android_floating_action_menu).setOnTouchListener(new View.OnTouchListener() {
-                private WindowManager.LayoutParams paramsF = parametersButton;
-                private int initialX;
-                private int initialY;
-                private float initialTouchX;
-                private float initialTouchY;
-
-                float move_X_axis,move_Y_axis;
-
-                @Override public boolean onTouch(View v, MotionEvent event) {
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_DOWN:
-
-                            Log.e("ACTION_DOWN","1");
-
-                            initialX = paramsF.x;
-                            initialY = paramsF.y;
-                            initialTouchX = event.getRawX();
-                            initialTouchY = event.getRawY();
-
-                            break;
-
-                        case MotionEvent.ACTION_UP:
-                            Log.e("ACTION_UP","2");
-                            break;
-
-                        case MotionEvent.ACTION_MOVE:
-                            Log.e("ACTION_MOVE","3");
-
-
-                            paramsF.x = initialX + (int) (event.getRawX() - initialTouchX);
-                            paramsF.y = initialY + (int) (event.getRawY() - initialTouchY);
-
-                            windowManager.updateViewLayout(layoutButton, paramsF);
-
-                            move_X_axis = event.getRawX() - initialTouchX;
-                            move_Y_axis = event.getRawY() - initialTouchY;
-
-                            lastPositionX = paramsF.x;
-                            lastPositionY = paramsF.y;
-
-                            break;
-                    }
-
-                    return false;
+        final FloatingActionMenu floatingActionMenu = (FloatingActionMenu) layoutButton.findViewById(R.id.material_design_android_floating_action_menu);
+        floatingActionMenu.setOnMenuButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(floatingActionMenu.isOpened()) {
+                    floatingActionMenu.close(true);
+                    floatingActionMenu.removeAllMenuButtons();
                 }
-            });
+                else {
+                    description.setButtonSize(1);
+                    description.setImageResource(R.mipmap.ic_pisi);
+                    screen.setButtonSize(1);
+                    screen.setImageResource(R.mipmap.ic_screen);
+                    video.setButtonSize(1);
+                    video.setImageResource(R.mipmap.ic_video);
+                    floatingActionMenu.addMenuButton(screen);
+                    floatingActionMenu.addMenuButton(video);
+                    floatingActionMenu.addMenuButton(description);
+                    floatingActionMenu.open(true);
+                }
+            }
+        });
 
-
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
-        /*layoutButton.setOnTouchListener(new View.OnTouchListener() {
+        //Button touched/moved/clicked
+        layoutButton.setOnTouchListener(new View.OnTouchListener() {
             WindowManager.LayoutParams updatedParameters = parametersButton;
             int x;
             int y;
@@ -185,8 +153,7 @@ public class FloatingWindow extends Service{
                 return false;
             }
 
-        });*/
-
+        });
     }
     //start timer function
     void startTimer() {
